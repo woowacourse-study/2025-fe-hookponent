f7ba26085f21970da2d6794140084f37d3f44fdd
hoyyChoi <hoyeon8165@gmail.com>
2025-07-19 14:37:37 +0900

    chore: testMatch 변경

--- packages/hooks/jest.config.ts
+++ packages/hooks/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: "jsdom",
-  testMatch: ["**/?(*.)+(test).ts?(x)"],
+  testMatch: ["**/?(*.)+(spec).ts?(x)"],
   transform: {
     "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
   },
