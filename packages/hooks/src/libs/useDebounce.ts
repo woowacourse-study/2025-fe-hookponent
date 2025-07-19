62bce0e9a40a137aa586bf2917aa55a3b3c54f95
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-19 20:58:06 +0900

    refactor: 다양한 callback 형태 대응

--- packages/hooks/src/libs/useDebounce.ts
+++ packages/hooks/src/libs/useDebounce.ts
@@ -1,20 +1,23 @@
 import { useCallback, useEffect, useRef } from 'react';
 
-type UseDebounceReturn = () => void;
+type UseDebounceReturn<T extends any[]> = (...args: T) => any;
 
-export function useDebounce(callback: () => void, wait: number): UseDebounceReturn {
+export function useDebounce<T extends any[]>(callback: (...args: T) => any, wait: number): UseDebounceReturn<T> {
   const timerIdRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const callbackRef = useRef(callback);
 
-  const debounced = useCallback(() => {
-    if (timerIdRef.current) {
-      clearTimeout(timerIdRef.current);
-    }
+  const debounced = useCallback(
+    (...args: T) => {
+      if (timerIdRef.current) {
+        clearTimeout(timerIdRef.current);
+      }
 
-    timerIdRef.current = setTimeout(() => {
-      callbackRef.current();
-    }, wait);
-  }, [wait]);
+      timerIdRef.current = setTimeout(() => {
+        callbackRef.current(...args);
+      }, wait);
+    },
+    [wait]
+  );
 
   useEffect(() => {
     callbackRef.current = callback;
