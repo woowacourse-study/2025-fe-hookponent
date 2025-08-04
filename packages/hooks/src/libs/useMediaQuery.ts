8883bf1f91bc39ca9c4f7757bba31585fa4d856e
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-08-04 09:48:16 +0900

    feat: 타입 확장

--- packages/hooks/src/libs/useMediaQuery.ts
+++ packages/hooks/src/libs/useMediaQuery.ts
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
 
-export function useMediaQuery(query: string): boolean {
-  const [matches, setMatches] = useState(false);
+export function useMediaQuery(query: string): boolean;
+export function useMediaQuery(query: string[]): boolean[];
+
+export function useMediaQuery(query: string | string[]): boolean | boolean[] {
+  const [matches, setMatches] = useState(() => {
+    if (Array.isArray(query)) return Array<boolean>(query.length).fill(false);
+    return false;
+  });
 
   useEffect(() => {
-    const media = window.matchMedia(query);
-
     const listener = () => {
-      setMatches(media.matches);
+      if (Array.isArray(query)) {
+        const queryMatches = query.map((eachQuery) => window.matchMedia(eachQuery).matches);
+        setMatches(queryMatches);
+      } else {
+        const media = window.matchMedia(query);
+        setMatches(media.matches);
+      }
     };
 
     window.addEventListener('resize', listener);
