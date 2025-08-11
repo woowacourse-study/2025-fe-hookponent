e76df1c1b8374665a75192f33d74fac32d7decbc
deokbin <deokbin@naver.com>
2025-08-11 18:34:58 +0900

    feat: useInfiniteScroll 훅 제작

---
+++ packages/hooks/src/libs/useInfiniteScroll.ts
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+
+export interface UseInfiniteScrollOptions {
+  callback: () => void;
+  options?: IntersectionObserverInit;
+}
+
+
+
+/**
+ * IntersectionObserver를 이용해 무한 스크롤 기능을 제공하는 커스텀 훅
+ *
+ * @param {UseInfiniteScrollOptions} params 무한 스크롤 옵션
+ * @param {() => void} params.callback 관찰 대상이 화면에 나타났을 때 실행할 콜백
+ * @param {IntersectionObserverInit} [params.options] IntersectionObserver 설정 값 (선택)
+ * @returns {React.RefObject<HTMLDivElement>} 감시 대상에 연결할 ref 객체
+ */
+
+export function useInfiniteScroll({
+  callback,
+  options = { root: null, rootMargin: '0px', threshold: 1.0 },
+}: UseInfiniteScrollOptions) {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!targetRef.current) return;
+    const observer = new IntersectionObserver(async (entries) => {
+      if (entries[0].isIntersecting && !loading) {
+        setLoading(true);
+        try {
+          await callback();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }, options);
+
+    observer.observe(targetRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [callback, options, loading]);
+
+  return [targetRef, loading];
+}
