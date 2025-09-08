ff16c429c41b048609554ef86a9563c84aa18636
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-09-09 00:59:18 +0900

    feat: paste, reset 기능 확장

--- packages/hooks/src/libs/useClipboard.ts
+++ packages/hooks/src/libs/useClipboard.ts
@@ -1,22 +1,69 @@
 import { useCallback, useState } from 'react';
 
-export function useClipboard() {
-  const [isCopied, setIsCopied] = useState(false);
+interface UseClipBoardReturns {
+  isCopied: boolean;
+  clipboardText: string | null;
+  error: Error | null;
 
-  const copy = useCallback(async (text: string) => {
-    if (!navigator?.clipboard) {
-      console.warn('Clipboard API not supported');
-      return;
-    }
+  copy: (text: string) => Promise<void>;
+  paste: () => Promise<string | null>;
+  reset: () => void;
+}
+
+export function useClipboard(timeout: number = 2000): UseClipBoardReturns {
+  const [isCopied, setIsCopied] = useState(false);
+  const [clipboardText, setClipboardText] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const copy = useCallback(
+    async (text: string) => {
+      if (!navigator?.clipboard) {
+        console.warn('Clipboard API not supported');
+        return;
+      }
+
+      try {
+        await navigator.clipboard.writeText(text);
+        setIsCopied(true);
+        setClipboardText(text);
+        setError(null);
+
+        setTimeout(() => {
+          setIsCopied(false);
+        }, timeout);
+      } catch (error) {
+        console.error('Failed to copy text: ', error);
+        setIsCopied(false);
+        setClipboardText(null);
+        setError(error as Error);
+      }
+    },
+    [timeout]
+  );
+
+  const paste = useCallback(async () => {
+    if (!navigator?.clipboard) return null;
 
     try {
-      await navigator.clipboard.writeText(text);
-      setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 2000);
+      const text = await navigator.clipboard.readText();
+      setClipboardText(text);
+      setError(null);
+
+      return text;
     } catch (error) {
-      console.error('Failed to copy text: ', error);
+      console.error('Failed to read clipboard: ', error);
+      setClipboardText(null);
+      setError(error as Error);
+
+      return null;
     }
   }, []);
 
-  return { isCopied, copy };
+  const reset = useCallback(() => {
+    setIsCopied(false);
+    setClipboardText(null);
+    setError(null);
+  }, []);
+
+  return { isCopied, clipboardText, error, copy, paste, reset };
 }
